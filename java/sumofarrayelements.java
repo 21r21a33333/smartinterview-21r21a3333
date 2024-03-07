84d9559706b3451023a58aab13c5e5bdccac4406
Karthikeya T <128052246+Karthik0131@users.noreply.github.com>
2024-03-07 20:33:48 +0530

    Create sumofarrayelements.java

---
+++ java/sumofarrayelements.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            long sum=0;
+            for(int i=0;i<n;i++){
+                sum+=sc.nextLong();;
+            }
+            System.out.println(sum);
+        }
+    }
+}
